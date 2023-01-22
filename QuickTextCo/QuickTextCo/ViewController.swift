5686b9c992956c3c1e01b0a0ec49510fc0f3be39
Abdullah Al-Ashi <abdullah.alashi@gmail.com>
2023-01-22 12:47:42 +1100

    - create new project with extension

---
+++ QuickTextCo/QuickTextCo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickTextCo
+//
+//  Created by Abdullah Al-Ashi on 21/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
