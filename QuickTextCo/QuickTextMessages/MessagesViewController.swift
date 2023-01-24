cb3d536c41c950ab85c4621016ba8398b63b582e
Abdullah Al-Ashi <abdullah.alashi@gmail.com>
2023-01-25 00:54:42 +1100

    - create app group - create a common service to store models

--- QuickTextCo/QuickTextMessages/MessagesViewController.swift
+++ QuickTextCo/QuickTextMessages/MessagesViewController.swift
@@ -13,6 +13,7 @@ class MessagesViewController: MSMessagesAppViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        iMessageService.shared.loadiMessage()
     }
     
     // MARK: - Conversation Handling
